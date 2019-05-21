f515940b05df25930e34c3ecf6e9d8a69ebe7982
Sam Guerrero <samkguerrero@gmail.com>
2019-05-21 10:34:22 -0700

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=wolfexpertassistant-Web-Deploy.pubxml -p:Password=MWdRw6bki8L3rzfqsk4erYxtMm8cgBwuP8EoZvPp5gkRJDGMYr4o4cdHvoQl
+
